3b3d73b2d9e78aa0bc6dd2a67446b991112b3633
Sara Stanton <sara.mpa13@gmail.com>
2020-08-15 02:54:26 -0500

    Flip ALMOST working (images still hardcoded)

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach((card => {
+    card.addEventListener('click', (event) => {
+      card.classList.toggle('is-flipped');
+      setTimeout(() => {
+        card.classList.toggle('is-flipped')
+      }, 1500)
+    })
+  }))
+
 
 })
